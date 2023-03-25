339eecae4d3ee5fd2e5853ad860d457d2b6afe29
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-25 16:33:34 +0100

    search added

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import styles from "./Search.module.css";
+import { useState } from "react";
 
 export default function Search() {
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <>
-            <button className={styles.button}>Search</button>
+            <button
+                className={styles.button}
+                onClick={() => setShowModal(!showModal)}
+            >
+                Search
+            </button>
+            {showModal && (
+                <>
+                    <div
+                        className={styles.modal}
+                        onClick={() => setShowModal(false)}
+                    ></div>
+                    <div className={styles.modalContent}>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            placeholder="Search..."
+                        />
+                    </div>
+                </>
+            )}
         </>
     );
 }
