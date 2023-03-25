82c0af0e71505cfb905d41d70a3d8870e9c8caba
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-25 15:34:11 +0100

    add movie lists

--- src/app/components/MovieLists/MovieLists.tsx
+++ src/app/components/MovieLists/MovieLists.tsx
@@ -6,10 +6,22 @@ import { client } from "@/lib/client";
 import { MY_EMAIL_KEY } from "@/constants/email";
 import { useState } from "react";
 import { MovieList } from "@/types/MovieList";
+import { useRef } from "react";
 
 const GetMovieLists = gql`
     query GetMovieLists($email: String!) {
         getMovieLists(email: $email) {
+            id
+            created_at
+            name
+            email
+        }
+    }
+`;
+
+const CreateList = gql`
+    mutation CreateList($input: CreateListInput!) {
+        createList(input: $input) {
             id
             created_at
             name
@@ -28,17 +40,53 @@ async function getMovieLists() {
     return getMovieLists;
 }
 
+async function createList(name: string) {
+    const { createList } = await client.request<{
+        createList: MovieList;
+    }>(CreateList, {
+        input: {
+            name: name,
+            email: MY_EMAIL_KEY,
+        },
+    });
+
+    return createList;
+}
+
 export default function MovieLists() {
     const [movieList, setMovieList] = useState<MovieList[]>([]);
+
+    const movieListName = useRef<HTMLInputElement>(null);
 
     getMovieLists().then((data) => {
         setMovieList(data);
     });
 
+    function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if (movieListName.current) {
+            createList(movieListName.current.value).then((data) => {
+                setMovieList([...movieList, data]);
+            });
+        }
+    }
+
     return (
         <main className={styles.main}>
             <h1> Movie lists: </h1>
-            {movieList.map((movieList) => (
+            <form className={styles.form} onSubmit={(e) => handleOnSubmit(e)}>
+                <input
+                    ref={movieListName}
+                    className={styles.input}
+                    type="text"
+                    placeholder="Movie list name"
+                />
+                <button className={styles.button} type="submit">
+                    Create
+                </button>
+            </form>
+            {movieList.reverse().map((movieList) => (
                 <div className={styles.movie_list} key={movieList.id}>
                     <h2> {movieList.name} </h2>
                 </div>
